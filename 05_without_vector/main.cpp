18d9471c0704fa823def9cd25f8576dfdf7fc71c
VadimTribrat <55830625+VadimTribrat@users.noreply.github.com>
2020-04-05 14:05:51 +0300

    Add files via upload

---
+++ 05_without_vector/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "mylib.h"
+
+using namespace std;
+
+int main(int argc, char * * argv)
+{
+	bigInt big("-1234567890000");
+	cout << big << endl;
+	cout << big << '+' << big << " = " << big+big << endl;
+	cout << big << "-" << bigInt("123") << " = " << big-bigInt("123") << endl;
+	cout << bigInt("1000000000000") << "-" << bigInt("1") << " = " << bigInt("1000000000000")-bigInt("1m") << endl;
+	cout << "-(" << big << ") = " << -big << endl;
+	cout << "-(" << big << ") - " << big << " = " << -big - big << endl;
+	cout << bigInt("123456") << " - " << bigInt("321") << " = " << bigInt("123456") - bigInt("321") << endl;
+	cout << bigInt("321") << " - " << bigInt("123456") << " = " << bigInt("321") - bigInt("123456") << endl;
+	cout << bigInt("321") << " < " << bigInt("123456") << " : " << (bigInt("321") < bigInt("123456")) << endl;
+	cout << bigInt("321") << " > " << bigInt("123456") << " : " << (bigInt("321") > bigInt("123456")) << endl;
+	cout << bigInt("-9999999999321") << " <= " << bigInt("1234560000000000") << " : " << (bigInt("-9999999999321") <= bigInt("1234560000000000")) << endl;
+	cout << bigInt("-321") << " == " << bigInt("321") << " : " << (bigInt("-321") == bigInt("321")) << endl;
+	cout << bigInt("321") << " == " << bigInt("321") << " : " << (bigInt("321") == bigInt("321")) << endl;
+	cout << bigInt("321") << " != " << bigInt("321") << " : " << (bigInt("321") != bigInt("321")) << endl;
+
+	return 0;
+}
