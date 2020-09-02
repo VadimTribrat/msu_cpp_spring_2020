090cfd54694dc422b6cebaf2f18c0718293d81d8
VadimTribrat <55830625+VadimTribrat@users.noreply.github.com>
2020-09-02 14:38:06 +0300

    Delete mylib.h

--- 02/mylib.h
+++
@@ -1,8 +0,0 @@
-using On = void (*)(const std::string);
-using At = void (*)();
-using num = void (*)(int);
-void register_on_string(On);
-void register_first(At);
-void register_end(At);
-void register_on_number(num);
-void parse(const char *);
